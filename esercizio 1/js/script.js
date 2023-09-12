1abc9df8f32b7d9f4f887b11df95aad7be8664a0
FraRussotto <russottofrancesco1@gmail.com>
2023-09-12 15:43:12 +0200

    [ADD] Version witch stamp

--- esercizio 1/js/script.js
+++ esercizio 1/js/script.js
@@ -3,7 +3,6 @@ const listUser = ["ciccio@ugo.com", "pippo@baudo.com", "mario@rossi.com"]
 
 // Creo la variabile che contiene l'email inserita manualmente dall'utente che vuole accedere
 const emailUser = prompt("Inserisci la tua email")
-console.log(emailUser)
 
 // Inserisco una variabile Booleana
 let match = false;
@@ -13,12 +12,12 @@ for(i = 0; i < listUser.length; i++){
   // Se l'email inserita è contenuta nella lista
   if(emailUser === listUser[i]){
     match = true;
-    console.log("Puoi entrare")
+    document.querySelector(".answer").innerHTML = "Puoi entrare";
   }
 }
 // Se l'email inserita non è contenuta nella lista
 if(match === false){
-  console.log("Non puoi entrare")
+  document.querySelector(".answer").innerHTML = "Non puoi entrare";
 }
 
 
